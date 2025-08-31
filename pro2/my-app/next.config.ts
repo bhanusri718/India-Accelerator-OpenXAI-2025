3211e1a04f1be1ca6ea155915d3754eb68a6c858
bhanusri718 <bhanusribyri695@gmail.com>
2025-08-31 10:56:35 +0530

    Set outputFileTracingRoot to fix lockfile warning

--- pro2/my-app/next.config.ts
+++ pro2/my-app/next.config.ts
@@ -1,5 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+// next.config.ts
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  outputFileTracingRoot: "C:/Users/YESHWANTH/India-Accelerator-OpenXAI-2025/pro2/my-app",
   allowedDevOrigins: ["http://192.168.1.179"],
 };
 
